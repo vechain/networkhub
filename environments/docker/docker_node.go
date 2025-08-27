cb4fcadd02948171559ed08c4cc7bcf82e0b07a8
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2025-08-27 10:01:15 +0100

    Darren/fix/large start command (#48)

--- environments/docker/docker_node.go
+++ environments/docker/docker_node.go
@@ -5,11 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 	"github.com/vechain/networkhub/network/node"
@@ -17,24 +20,45 @@ import (
 )
 
 // NewDockerNode initializes a new DockerNode
-func NewDockerNode(cfg node.Config, enodes []string, networkID string, exposedPorts *exposedPort, ipAddr string) *Node {
+func NewDockerNode(
+	cfg node.Config,
+	enodes []string,
+	networkID string,
+	hostPort string,
+	containerPort string,
+	ipAddr string,
+) *Node {
 	return &Node{
-		cfg:          cfg,
-		enodes:       enodes,
-		networkID:    networkID,
-		exposedPorts: exposedPorts,
-		ipAddr:       ipAddr,
+		cfg:           cfg,
+		enodes:        enodes,
+		networkID:     networkID,
+		hostPort:      hostPort,
+		containerPort: containerPort,
+		ipAddr:        ipAddr,
 	}
 }
 
 // Node represents a Docker container node
 type Node struct {
-	cfg          node.Config
-	enodes       []string
-	id           string
-	networkID    string
-	exposedPorts *exposedPort
-	ipAddr       string
+	cfg           node.Config
+	enodes        []string
+	id            string
+	networkID     string
+	hostPort      string
+	containerPort string
+	ipAddr        string
+	hostDataDir   string
+	logConfig     container.LogConfig
+}
+
+// SetHostVolume sets the base directory on the host where node data will be stored.
+// The system will automatically create subdirectories: {hostDataDir}/{nodeID}/config and {hostDataDir}/{nodeID}/data
+func (n *Node) SetHostVolume(hostDataDir string) {
+	n.hostDataDir = hostDataDir
+}
+
+func (n *Node) SetLogConfig(logConfig container.LogConfig) {
+	n.logConfig = logConfig
 }
 
 // Start runs the node as a Docker container
@@ -71,7 +95,7 @@ func (n *Node) Start() error {
 		}
 	}
 
-	cleanEnode := []string{} // todo theres a clever way of doing this
+	var cleanEnode []string // todo theres a clever way of doing this
 	for _, enode := range n.enodes {
 		nodeEnode, err := n.cfg.Enode(n.ipAddr)
 		if err != nil {
@@ -83,57 +107,120 @@ func (n *Node) Start() error {
 	}
 	enodeString := strings.Join(cleanEnode, ",")
 
-	args := "cd /home/thor; " +
-		"echo $GENESIS > genesis.json;" +
-		"echo $PRIVATEKEY > master.key;" +
-		"echo $PRIVATEKEY > p2p.key;" +
-		"thor " +
-		"--network genesis.json " +
-		"--nat none " +
-		fmt.Sprintf("--config-dir='%s' ", n.cfg.GetConfigDir()) +
-		fmt.Sprintf("--api-addr='%s' ", n.cfg.GetAPIAddr()) +
-		fmt.Sprintf("--api-cors='%s' ", n.cfg.GetAPICORS()) +
-		fmt.Sprintf("--p2p-port=%d ", n.cfg.GetP2PListenPort()) +
-		fmt.Sprintf("--bootnode=%s", enodeString)
-
-	for key, value := range n.cfg.GetAdditionalArgs() {
-		args += fmt.Sprintf(" --%s=%s", key, value)
+	if n.hostDataDir == "" {
+		n.hostDataDir = os.TempDir()
+		slog.Info("host data directory not specified, using temporary directory", "dir", n.hostDataDir)
 	}
+	baseHostDir := fmt.Sprintf("%s/%s", n.hostDataDir, n.cfg.GetID())
 
-	cmd := []string{"sh", "-c", args}
-
-	//serialize genesis
+	// write the genesis to the config directory on the host
 	genesisBytes, err := nodegenesis.Marshal(n.cfg.GetGenesis())
 	if err != nil {
 		return fmt.Errorf("unable to marshal genesis - %w", err)
 	}
+	configDir := fmt.Sprintf("%s/config", baseHostDir)
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		return fmt.Errorf("failed to create config directory %s: %w", configDir, err)
+	}
+	genesisPath := fmt.Sprintf("%s/genesis.json", configDir)
+	if err := os.WriteFile(genesisPath, genesisBytes, 0644); err != nil {
+		return fmt.Errorf("failed to write genesis file to %s: %w", genesisPath, err)
+	}
+
+	// write the master key to the config directory on the host
+	masterKeyPath := fmt.Sprintf("%s/master.key", configDir)
+	if err := os.WriteFile(masterKeyPath, []byte(n.cfg.GetKey()), 0600); err != nil {
+		return fmt.Errorf("failed to write master key to %s: %w", masterKeyPath, err)
+	}
+	// write the p2p key to the config directory on the host
+	p2pKeyPath := fmt.Sprintf("%s/p2p.key", configDir)
+	if err := os.WriteFile(p2pKeyPath, []byte(n.cfg.GetKey()), 0600); err != nil {
+		return fmt.Errorf("failed to write p2p key to %s: %w", p2pKeyPath, err)
+	}
+
+	containerConfigDir := n.cfg.GetConfigDir()
+	if strings.HasSuffix(containerConfigDir, "/") {
+		containerConfigDir = strings.TrimSuffix(containerConfigDir, "/")
+	}
+
+	var cmd strslice.StrSlice
+	cmd = append(cmd, "--network", containerConfigDir+"/genesis.json")
+	cmd = append(cmd, "--nat", "none")
+	cmd = append(cmd, "--config-dir", n.cfg.GetConfigDir())
+	cmd = append(cmd, "--api-addr", n.cfg.GetAPIAddr())
+	cmd = append(cmd, "--api-cors", n.cfg.GetAPICORS())
+	cmd = append(cmd, "--p2p-port", fmt.Sprintf("%d", n.cfg.GetP2PListenPort()))
+
+	if enodeString != "" {
+		cmd = append(cmd, "--bootnodes", enodeString)
+	}
+
+	for key, value := range n.cfg.GetAdditionalArgs() {
+		cmd = append(cmd, "--"+key, value)
+	}
 
 	exposedPorts := nat.PortSet{
-		nat.Port(fmt.Sprintf("%s/tcp", n.exposedPorts.containerPort)): struct{}{},
+		nat.Port(fmt.Sprintf("%s/tcp", n.containerPort)): struct{}{},
 	}
 	portBindings := map[nat.Port][]nat.PortBinding{
-		nat.Port(fmt.Sprintf("%s/tcp", n.exposedPorts.containerPort)): {
+		nat.Port(fmt.Sprintf("%s/tcp", n.containerPort)): {
 			{
-				HostPort: n.exposedPorts.hostPort,
+				HostPort: n.hostPort,
 			},
 		},
 	}
 
 	// Construct Docker container configuration
 	config := &container.Config{
-		Image:      n.cfg.GetExecArtifact(),
-		Cmd:        cmd,
-		Entrypoint: []string{},
-		Env: []string{
-			fmt.Sprintf("GENESIS=%s", string(genesisBytes)),
-			fmt.Sprintf("PRIVATEKEY=%s", n.cfg.GetKey()),
+		Image: n.cfg.GetExecArtifact(),
+		Cmd:   cmd,
+		Entrypoint: []string{
+			"thor",
 		},
 		ExposedPorts: exposedPorts,
 	}
 
 	hostConfig := &container.HostConfig{
 		PortBindings: portBindings,
+		LogConfig:    n.logConfig,
 	}
+
+	config.Volumes = make(map[string]struct{})
+	var binds []string
+
+	nodeID := n.cfg.GetID()
+
+	// Mount config directory: eg /host/nodes/{nodeID}/config -> container's config dir
+	if configDir := n.cfg.GetConfigDir(); configDir != "" {
+		hostConfigPath := fmt.Sprintf("%s/config", baseHostDir)
+
+		// Create the host directory if it doesn't exist
+		if err := os.MkdirAll(hostConfigPath, 0755); err != nil {
+			return fmt.Errorf("failed to create config directory %s: %w", hostConfigPath, err)
+		}
+
+		volumeBind := fmt.Sprintf("%s:%s", hostConfigPath, configDir)
+		config.Volumes[configDir] = struct{}{}
+		binds = append(binds, volumeBind)
+	}
+
+	// Mount data directory: eg /host/nodes/{nodeID}/data -> container's data dir
+	if dataDir := n.cfg.GetDataDir(); dataDir != "" {
+		hostDataPath := fmt.Sprintf("%s/data", baseHostDir)
+
+		// Create the host directory if it doesn't exist
+		if err := os.MkdirAll(hostDataPath, 0755); err != nil {
+			return fmt.Errorf("failed to create data directory %s: %w", hostDataPath, err)
+		}
+
+		volumeBind := fmt.Sprintf("%s:%s", hostDataPath, dataDir)
+		config.Volumes[dataDir] = struct{}{}
+		binds = append(binds, volumeBind)
+	}
+
+	slog.Info("setting up volume binds for node", "nodeID", nodeID, "binds", strings.Join(binds, ", "))
+
+	hostConfig.Binds = binds
 
 	// Define the network configuration
 	networkConfig := &network.NetworkingConfig{
