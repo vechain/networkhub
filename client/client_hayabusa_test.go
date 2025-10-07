a13b49b85a48aea1ceb10311b360e53e5ad154e1
Pedro Gomes <otherview@gmail.com>
2025-10-07 13:56:04 +0100

    Update client/client_hayabusa_test.go

--- client/client_hayabusa_test.go
+++ client/client_hayabusa_test.go
@@ -24,7 +24,7 @@ func TestClientFourNodesHayabusa(t *testing.T) {
 	fourNodesHayabusaNetwork := preset.LocalFourNodesHayabusa()
 	fourNodesHayabusaNetwork.ThorBuilder.DownloadConfig = &thorbuilder.DownloadConfig{
 		RepoUrl:    "https://github.com/vechain/thor",
-		Branch:     "pedro/hayabusa/improve_customnet",
+		Branch:     "release/hayabusa",
 		IsReusable: false,
 	}
 
